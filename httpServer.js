bb021b6af3fdb64cd55333238700490b7afdb590
jbays <jjbaize@gmail.com>
2018-05-21 13:39:12 -0700

    Pushing up solutions for httpServer

--- httpServer.js
+++ httpServer.js
@@ -6,8 +6,8 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
   const petRegExp = /^\/pets\/(.*)$/;
 
-
   if ( req.method === 'GET') {
+    //request all pets
     if ( req.url === '/pets' ) {
       fs.readFile(petsPath, 'utf8', (err, petsJSON)=>{
         if (err) {
@@ -17,9 +17,11 @@ const server = http.createServer((req,res)=>{
           return res.end('Internal Server Error');
         }
         res.setHeader('Content-Type', 'application/json');
-        res.end(petsJSON);
+        return res.end(petsJSON);
       })
-    } else if ( petRegExp.test(req.url) ){
+    }
+    //request a specific pet @ index
+    else if ( petRegExp.test(req.url) ){
       fs.readFile(petsPath, 'utf8', (err, petsJSON)=>{
         if (err) {
           console.error(err.stack);
@@ -44,7 +46,60 @@ const server = http.createServer((req,res)=>{
         return res.end(petJSON)
       })
     }
+    //POST
+  } else if ( req.method === "POST" && req.url === '/pets' ) {
+    let jsonBody = '';
 
+    req.on('data', (chunk)=>{
+      jsonBody += chunk.toString();
+    })
+
+    req.on('end',()=>{
+
+      fs.readFile(petsPath, 'utf8', (readErr, petsJSON)=>{
+        if (readErr) {
+          console.error(readErr.stack);
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'text/plain');
+          return res.end('Internal Service Error');
+        }
+
+        const body = JSON.parse(jsonBody);
+        const pets = JSON.parse(petsJSON);
+        const age  = Number.parseInt(body.age);
+        const kind = body.kind;
+        const name = body.name;
+
+        if ( Number.isNaN(age) || !kind || !name ) {
+          res.statusCode = 400;
+          res.setHeader('Content-Type','text/plain');
+          return res.end('Bad Request');
+        }
+
+        const pet = {age,kind,name};
+        pets.push(pet);
+
+        const petJSON = JSON.stringify(pet);
+        const newPetsJSON = JSON.stringify(pets);
+
+        fs.writeFile(petsPath, newPetsJSON, (writeErr)=>{
+          if (writeErr){
+            console.error(writeErr.stack);
+            res.statusCode = 500;
+            res.setHeader('Content-Type','text/plain');
+            return res.end('Internal Service Error');
+          }
+          res.setHeader('Content-Type','application/json');
+          return res.end(petJSON)
+        })
+
+      })
+    })
+
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type','text/plain');
+    return res.end('Not Found');
   }
 })
 
